766cc86f5e98aa213db813f603a91545763e293f
mawri <abhinav210990@gmail.com>
2016-04-06 05:46:07 +0530

    final changes 6/4 morning

--- src/com/local/dfs/client/Client.java
+++ src/com/local/dfs/client/Client.java
@@ -1,9 +1,11 @@
 package com.local.dfs.client;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.rmi.NotBoundException;
@@ -39,7 +41,7 @@ public class Client {
 
 	// you know what this means
 	static int blockSize;
-	
+
 	// static status messages
 	static final int SUCCESS = 0;
 	static final int FAIL = 1;
@@ -50,10 +52,10 @@ public class Client {
 	// registry lookup strings
 	private static String NNString = "NameNode";
 	private static String DNString = "DataNode";
-	
+
 	// maintain book of open files
 	private static HashMap<String,Integer> openfiles=new HashMap<String, Integer>();
-	
+
 	public static void main(String[] args) 
 	{
 		if (args.length < 1) 
@@ -61,7 +63,7 @@ public class Client {
 			System.out.println("Too few arguments. Provide the configuration file!");
 			System.exit(0);
 		}
-		
+
 		Client client = new  Client();
 		try 
 		{
@@ -76,42 +78,44 @@ public class Client {
 		{
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	/**
 	 * @throws NotBoundException
 	 * @throws IOException
 	 */
-	private void run() throws NotBoundException, IOException {
+	private void run() throws NotBoundException, IOException 
+	{
+		System.out.println("Client started running! get, put, list, exit");
 		Scanner scanner = new Scanner(new InputStreamReader(System.in));	
-		
+
 		while (true) {
 			String line = scanner.nextLine();
 			String cmd = StringUtility.getSegment(0, line, " ");
 			String fileName = null;
-			
+
 			switch (cmd) {
-				case "get":
-					fileName = StringUtility.getSegment(1, line, " ");
-					this.read(fileName);
-					break;
-				case "put":
-					fileName = StringUtility.getSegment(1, line, " ");
-					this.write(fileName);
-					break;
-				case "list":
-					this.listFile();
-					break;
-				case "exit":
-					scanner.close();
-					System.exit(0);
-				default:
-					// do nothing
+			case "get":
+				fileName = StringUtility.getSegment(1, line, " ");
+				this.read(fileName);
+				break;
+			case "put":
+				fileName = StringUtility.getSegment(1, line, " ");
+				this.write(fileName);
+				break;
+			case "list":
+				this.listFile();
+				break;
+			case "exit":
+				scanner.close();
+				System.exit(0);
+			default:
+				// do nothing
 			}
 		}
 	}
-	
+
 	/**
 	 * Initializes by reading config file and looks up registry
 	 * @param configFile
@@ -140,7 +144,7 @@ public class Client {
 		// now lookup remote object in the registry
 		nnRegistry = LocateRegistry.getRegistry(nnIP, nnPort);
 		nnStub = (INameNode) nnRegistry.lookup(NNString);
-		
+
 		br.close();
 	}
 
@@ -155,7 +159,7 @@ public class Client {
 		req.setDirName("");
 
 		ListFilesResponse response = ListFilesResponse.parseFrom(nnStub.list(req.build().toByteArray()));
-		
+
 		if (response.getStatus() == SUCCESS)
 		{
 			System.out.println("List of Files - ");
@@ -170,7 +174,7 @@ public class Client {
 		}
 	}
 
-	
+
 	/**
 	 * @param fileName
 	 * @throws NotBoundException
@@ -182,80 +186,100 @@ public class Client {
 		OpenFileRequest.Builder openReq = OpenFileRequest.newBuilder();
 		openReq.setFileName(fileName);
 		openReq.setForRead(true);
-		
+
 		OpenFileResponse openResponse = OpenFileResponse.parseFrom(nnStub.openFile(openReq.build().toByteArray()));
-		
+
 		// file read possible
 		if (openResponse.getStatus() == SUCCESS)
 		{
 			int handle = openResponse.getHandle();
-			
+
 			// update book
 			openfiles.put(fileName, handle);
-			
+
 			// now read
 			BlockLocationRequest.Builder blockLocReq = BlockLocationRequest.newBuilder();
-			
+
+			System.out.println("openResponse.getBlockNumsList(): " + openResponse.getBlockNumsList());  	// TODO remove
+
 			// add the block numbers to the blockLocRequest
 			for (int bNo : openResponse.getBlockNumsList())
 			{
 				blockLocReq.addBlockNums(bNo);
 			}
-			
+
+			System.out.println ("blockReq.getBlockNumsList: " + blockLocReq.getBlockNumsList());		// TODO remove
 			BlockLocationResponse blockLocResp = 
 					BlockLocationResponse.parseFrom(nnStub.getBlockLocations(blockLocReq.build().toByteArray()));
-			
+
+			System.out.println("Read block locations: " + blockLocResp.getBlockLocationsList().toString());		// TODO remove
+			System.out.println(blockLocResp.getStatus());				// TODO  remove
+			System.out.println("Block locations count: " + blockLocResp.getBlockLocationsCount());		// TODO remove
+
 			if (blockLocResp.getStatus() == SUCCESS)
 			{
-				for (int i = 0; i < blockLocResp.getBlockLocationsCount(); i++)
+				// delete if exists
+				File tmp = new File("output");
+				if (tmp.exists())
 				{
-					BlockLocations b = blockLocResp.getBlockLocations(i);
-					
-					int blockNo = b.getBlockNumber();
-					DataNodeLocation dnLoc = b.getLocations(0);		// okay to take the first one as we know it's up
-					
-					Registry dnReg = LocateRegistry.getRegistry(dnLoc.getIp(), dnLoc.getPort());
-					IDataNode dnStub = (IDataNode) dnReg.lookup(DNString);
+					tmp.delete();
+				}
 				
-					// now send the read request
-					ReadBlockRequest.Builder rbReq = ReadBlockRequest.newBuilder();
-					rbReq.setBlockNumber(blockNo);
-					
-					ReadBlockResponse rbResponse = 
-							ReadBlockResponse.parseFrom(dnStub.readBlock(rbReq.build().toByteArray()));
-					
-					if (rbResponse.getStatus() == SUCCESS)
+				// actual writing starts
+				try(FileOutputStream fw = new FileOutputStream("output", true))
+				{
+					for (int i = 0; i < blockLocResp.getBlockLocationsCount(); i++)
 					{
-						ByteString byteString = rbResponse.getData(0);
-						String data = new String(byteString.toByteArray(), "UTF-8");
-						
-						// write to local output file
-						FileWriter fw = new FileWriter(new File("output"), true);
-						fw.write(data);
-						fw.close();
+						BlockLocations b = blockLocResp.getBlockLocations(i);
+
+						int blockNo = b.getBlockNumber();
+						DataNodeLocation dnLoc = b.getLocations(0);		// okay to take the first one as we know it's up
+
+						Registry dnReg = LocateRegistry.getRegistry(dnLoc.getIp(), dnLoc.getPort());
+						IDataNode dnStub = (IDataNode) dnReg.lookup(DNString);
+
+						// now send the read request
+						ReadBlockRequest.Builder rbReq = ReadBlockRequest.newBuilder();
+						rbReq.setBlockNumber(blockNo);
+
+						ReadBlockResponse rbResponse = 
+								ReadBlockResponse.parseFrom(dnStub.readBlock(rbReq.build().toByteArray()));
+
+						if (rbResponse.getStatus() == SUCCESS)
+						{
+							System.out.println("ReadBlock response successful");	// TODO remove
+
+							ByteString byteString = rbResponse.getData(0);
+
+							// write to local output file
+							fw.write(byteString.toByteArray());
+
+							System.out.println("Written to file \"output\"");		// TODO remove
+						}
+						else
+						{
+							System.out.println("Read error: Unable to read block");
+						}
 					}
-					else
-					{
-						System.out.println("Read error: Unable to read block");
-					}
+
 				}
 			}
 			else
 			{
 				System.out.println("Read error : Unable to get block locations");
 			}
-			
+
 			// need to close the file
 			CloseFileRequest.Builder closeRequest = CloseFileRequest.newBuilder();
 			closeRequest.setHandle(handle);
-			
+
 			CloseFileResponse closeResponse = 
 					CloseFileResponse.parseFrom(nnStub.closeFile(closeRequest.build().toByteArray()));
-			
+
 			if (closeResponse.getStatus() == SUCCESS)
 			{
 				openfiles.remove(fileName);
-				System.out.println("File closed successfully!");
+				System.out.println("Read: File closed successfully!");
 			}
 			else
 			{
@@ -267,8 +291,8 @@ public class Client {
 			System.out.println("Read error : Unable to open file");
 		}
 	}
-	
-	
+
+
 	/**
 	 * @param fileName
 	 * @throws NotBoundException
@@ -280,97 +304,79 @@ public class Client {
 		OpenFileRequest.Builder openReq = OpenFileRequest.newBuilder();
 		openReq.setFileName(fileName);
 		openReq.setForRead(false);
-		
+
 		OpenFileResponse openResponse = OpenFileResponse.parseFrom(nnStub.openFile(openReq.build().toByteArray()));
-		
+
 		// file read possible
 		if (openResponse.getStatus() == SUCCESS)
 		{
 			int handle = openResponse.getHandle();
-			
+
 			// update book
 			openfiles.put(fileName, handle);
-		
+
 			// get file size in MB
 			long MB = (long) Math.ceil((double) new File(fileName).length() / (1024 * 1024));
 			int nBlocks = (int) Math.ceil((double) MB / blockSize);
-			
-			BufferedReader br = new BufferedReader(new FileReader(new File (fileName)));
-			StringBuffer sb = new StringBuffer();
-			String line;
-			
-			for (int i = 0; i < nBlocks; i++)
-			{
-				int charsRead = 0;
-				sb.setLength(0);
-				
-				// request for assign block
-				AssignBlockRequest.Builder abReq = AssignBlockRequest.newBuilder();
-				abReq.setHandle(handle);
-				
-				AssignBlockResponse abResponse = 
-						AssignBlockResponse.parseFrom(abReq.build().toByteArray());
-				
-				// block assignment successful
-				if (abResponse.getStatus() == SUCCESS)
-				{
-					BlockLocations locations = abResponse.getNewBlock();
-					DataNodeLocation primaryDN = locations.getLocations(0);
-					
-					// remote method call
-					Registry dnReg = LocateRegistry.getRegistry(primaryDN.getIp(), primaryDN.getPort());
-					IDataNode dnStub = (IDataNode) dnReg.lookup(DNString);
-					
-					// read in a loop
-					line = br.readLine();
-					charsRead += line.length() + 1;
-					while (line != null && charsRead <= (blockSize * 1024 * 1024))
-					{
-						sb.append(line).append('\n');
-						br.mark(charsRead);
-						line = br.readLine();
-						if (line != null) charsRead += line.length() + 1;
-					}
-					// case where file read continues but exceeds block size
-					if(line != null && charsRead > blockSize * 1024 * 1024)
-					{					
-						br.reset();
-					}
-					
-					byte[] readBytes = sb.toString().getBytes();
 
-					WriteBlockRequest.Builder wbReq = WriteBlockRequest.newBuilder();
-					wbReq.setBlockInfo(locations);
-					wbReq.addData(ByteString.copyFrom(readBytes));
-					
-					// calling remote
-					WriteBlockResponse wbResponse = 
-							WriteBlockResponse.parseFrom(dnStub.writeBlock(wbReq.build().toByteArray()));
-					
-					if (wbResponse.getStatus() == SUCCESS)
+			System.out.println("Blocks assigned: " + nBlocks);
+
+			byte[] buffer = new byte[blockSize * 1024 * 1024];		// assign a buffer for reading
+
+			File file = new File(fileName);							// open a file handle
+			try(BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file)))		// try blocks to avoid close
+			{
+				int tmp = 0;
+				while ((tmp = bis.read(buffer)) > 0)
+				{
+					// request for assign block
+					AssignBlockRequest.Builder abReq = AssignBlockRequest.newBuilder();
+					abReq.setHandle(handle);
+
+					AssignBlockResponse abResponse = 
+							AssignBlockResponse.parseFrom(nnStub.assignBlock(abReq.build().toByteArray()));
+
+					// block assignment successful
+					if (abResponse.getStatus() == SUCCESS)
 					{
-						System.out.println("Block Written!");
+						BlockLocations locations = abResponse.getNewBlock();
+						DataNodeLocation primaryDN = locations.getLocations(0);
+
+						// remote method call
+						Registry dnReg = LocateRegistry.getRegistry(primaryDN.getIp(), primaryDN.getPort());
+						IDataNode dnStub = (IDataNode) dnReg.lookup(DNString);
+
+						WriteBlockRequest.Builder wbReq = WriteBlockRequest.newBuilder();
+						wbReq.setBlockInfo(locations);
+						wbReq.addData(ByteString.copyFrom(buffer, 0, tmp));
+
+						// calling remote
+						WriteBlockResponse wbResponse = 
+								WriteBlockResponse.parseFrom(dnStub.writeBlock(wbReq.build().toByteArray()));
+
+						if (wbResponse.getStatus() == SUCCESS)
+						{
+							System.out.println("Block Written!");
+						}
+						else
+						{
+							System.out.println("Failure in wrinting block!");
+						}
 					}
 					else
 					{
-						System.out.println("Failure in wrinting block!");
+						System.out.println("Assign Block request failed!");
 					}
 				}
-				else
-				{
-					System.out.println("Assign Block request failed!");
-				}
 			}
-			
-			br.close();
-			
+
 			// need to close the file
 			CloseFileRequest.Builder closeRequest = CloseFileRequest.newBuilder();
 			closeRequest.setHandle(handle);
-			
+
 			CloseFileResponse closeResponse = 
 					CloseFileResponse.parseFrom(nnStub.closeFile(closeRequest.build().toByteArray()));
-			
+
 			if (closeResponse.getStatus() == SUCCESS)
 			{
 				openfiles.remove(fileName);
